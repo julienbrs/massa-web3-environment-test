4ab5da15eb01832c22462a99af03a482e48dfae8
julienbrs <106234742+julienbrs@users.noreply.github.com>
2023-06-06 13:44:28 +0200

    wails repo

---
+++ wails-react-ts/frontend/src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import MassaWeb3Test from './Web3Test'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <MassaWeb3Test />
+      </div>
+    </>
+  )
+}
+
+export default App
